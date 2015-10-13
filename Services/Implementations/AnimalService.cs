c328c233f5888476b7499d10fe29d1991e66e272
mcinteer <ryan.mcinteer@gmail.com>
2015-10-13 20:45:36 +1300

    Enable Cors for chargetypes and animals

--- Services/Implementations/AnimalService.cs
+++ Services/Implementations/AnimalService.cs
@@ -1,13 +1,29 @@
-﻿using Wizard.Api.Models;
+﻿using Wizard.Api.Adapters;
+using Wizard.Api.Models;
 using Wizard.Api.Services.Interfaces;
 
 namespace Wizard.Api.Services.Implementations
 {
     public class AnimalService : IAnimalService
     {
+        private readonly IStorageAdapter _storage;
+        public const string ContainerName = Codes.Azure.Containers.Horses;
+
+        public AnimalService(IStorageAdapter storage)
+        {
+            _storage = storage;
+        }
+
         public void Save(HorsesContract horses)
         {
-            throw new System.NotImplementedException();
+            var fileName = $"{horses.StableEmail}.json";
+            _storage.UploadText(fileName, horses.ToJson, ContainerName);
+        }
+
+        public string Get(string email)
+        {
+            var fileName = $"{email}.json";
+            return _storage.DownloadText(fileName, ContainerName);
         }
     }
 }
--- Services/Interfaces/IAnimalService.cs
+++ Services/Interfaces/IAnimalService.cs
@@ -5,5 +5,6 @@ namespace Wizard.Api.Services.Interfaces
     public interface IAnimalService
     {
         void Save(HorsesContract horses);
+        string Get(string email);
     }
 }
